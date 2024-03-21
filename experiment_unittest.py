00d5387c83357e98e9c9418a56fd6a2ef10bc92a
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2024-03-21 14:28:23 +0100

    action

--- experiment_unittest.py
+++ experiment_unittest.py
@@ -4,6 +4,6 @@ import main
 class RunTest(unittest.TestCase):
     def test_create_valid_object(self):
         main_object = main.create_object("Stan")
-        self.assertEquals(main_object.name, "Jeff")
+        self.assertEquals(main_object.name, "Stan")
 
 unittest.main()
